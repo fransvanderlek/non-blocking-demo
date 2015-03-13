1969298ff209325c14206cd60ba9879c45b72236
fransvanderlek <Frans.Lek@klm.com>
2015-03-13 15:08:26 +0100

    reset url

--- src/main/java/org/iisiplusone/nonblockingdemo/endpoint/DemoEndpointController.java
+++ src/main/java/org/iisiplusone/nonblockingdemo/endpoint/DemoEndpointController.java
@@ -37,7 +37,7 @@ public class DemoEndpointController {
     	final DeferredResult<String> deferredResult = new DeferredResult<String>();
     	final long invocationTime =  System.currentTimeMillis();
     	
-    	httpClient.execute(new HttpGet("http://www.nu.nl"), new FutureCallback<HttpResponse>() {
+    	httpClient.execute(new HttpGet("http://192.168.33.1:8082/demo/mock"), new FutureCallback<HttpResponse>() {
 			
 			
 			public void failed(Exception exception) {
